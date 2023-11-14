230a0d5cf1e888d1f0c46f948e0d360b4f104f6e
littlemars-guy <81258511+littlemars-guy@users.noreply.github.com>
2023-11-14 22:20:02 +0100

    Update FFMPEG - Video to ProRes.bat
    reworked loop function

--- FFMPEG - Video to ProRes.bat
+++ FFMPEG - Video to ProRes.bat
@@ -4,6 +4,9 @@
 ::	What follows is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 ::
 ::	---CHANGELOG-----------------------------------------------------------------------------------
+::	2023-14-10 Version 0.2.5
+::		Reworked loop behavior when encoding multiple files
+::		Updated banners
 ::	2023-14-10 Version 0.2.1
 ::		Partial banner update
 ::	2023-11-10 Version 0.2
@@ -21,17 +24,20 @@ cls
 	if exist "%~1.mov" goto:errorfile
 	if "%~1" == "" goto:done
 	::	Let's go!
-	echo ╔═══════════════════════════════════════════════════════════════════╗
-	echo ║  ooooooooo.                      ooooooooo.                       ║ 
-	echo ║  `888   `Y88.                    `888   `Y88.                     ║ 
-	echo ║   888   .d88' oooo d8b  .ooooo.   888   .d88'  .ooooo.   .oooo.o  ║ 
-	echo ║   888ooo88P'  `888""8P d88' `88b  888ooo88P'  d88' `88b d88(  "8  ║ 
-	echo ║   888          888     888   888  888`88b.    888ooo888 `"Y88b.   ║ 
-	echo ║   888          888     888   888  888  `88b.  888    .o o.  )88b  ║ 
-	echo ║  o888o        d888b    `Y8bod8P' o888o  o888o `Y8bod8P' 8""888P'  ║
-	echo ╚═══════════════════════════════════════════════════════════════════╝
+	echo           ╔═══════════════════════════════════════════════════════════════════╗
+	echo           ║  ooooooooo.                      ooooooooo.                       ║ 
+	echo           ║  `888   `Y88.                    `888   `Y88.                     ║ 
+	echo           ║   888   .d88' oooo d8b  .ooooo.   888   .d88'  .ooooo.   .oooo.o  ║ 
+	echo           ║   888ooo88P'  `888""8P d88' `88b  888ooo88P'  d88' `88b d88(  "8  ║ 
+	echo           ║   888          888     888   888  888`88b.    888ooo888 `"Y88b.   ║ 
+	echo           ║   888          888     888   888  888  `88b.  888    .o o.  )88b  ║ 
+	echo           ║  o888o        d888b    `Y8bod8P' o888o  o888o `Y8bod8P' 8""888P'  ║
+	echo           ╚═══════════════════════════════════════════════════════════════════╝
 	echo.
     echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
+	echo.
+	echo	NOTE:  this script can encode ProRes only up to 10bit precision, if you need
+	echo		   12bits you should use another encoder.
 	echo.
 	echo [101;93m ENCODER SELECTION [0m
 
@@ -55,6 +61,14 @@ cls
 	:PRProxy
 		cls
 		echo ╔═══════════════════════════════════════════════════════════════════════╗
+		echo ║  ooooooooo.                      ooooooooo.                           ║ 
+		echo ║  `888   `Y88.                    `888   `Y88.                         ║ 
+		echo ║   888   .d88' oooo d8b  .ooooo.   888   .d88'  .ooooo.   .oooo.o      ║ 
+		echo ║   888ooo88P'  `888""8P d88' `88b  888ooo88P'  d88' `88b d88(  "8      ║ 
+		echo ║   888          888     888   888  888`88b.    888ooo888 `"Y88b.       ║ 
+		echo ║   888          888     888   888  888  `88b.  888    .o o.  )88b      ║ 
+		echo ║  o888o        d888b    `Y8bod8P' o888o  o888o `Y8bod8P' 8""888P'      ║
+		echo ║                                                                       ║
 		echo ║  ooooooooo.   ooooooooo.     .oooooo.   ooooooo  ooooo oooooo   oooo  ║  
 		echo ║  `888   `Y88. `888   `Y88.  d8P'  `Y8b   `8888    d8'   `888.   .8'   ║  
 		echo ║   888   .d88'  888   .d88' 888      888    Y888..8P      `888. .8'    ║  
@@ -65,7 +79,9 @@ cls
 		echo ╚═══════════════════════════════════════════════════════════════════════╝
 		echo.
 		echo [101;93m ENCODING... [0m
-		echo. && echo.
+		echo Input: %~1
+		echo Output: %~dp1%~n1_ProResProxy.mov
+		echo.
 
 		::	Get codec name
         setlocal EnableDelayedExpansion
@@ -87,19 +103,46 @@ cls
 			-i "%~1" ^
 			-c:v prores_ks ^
 			-profile:v 0 ^
+			-quant_mat auto ^
 			-vendor apl0 ^
-			-bits_per_mb 8000 ^
+			-bits_per_mb 250 ^
 			-pix_fmt yuv422p10le ^
 			-c:a %codec_audio% ^
 			-map_metadata 0 ^
 			-movflags use_metadata_tags ^
 			"%~dp1%~n1_ProResProxy.mov"
-		GOTO:ENDOFPRORES
+			if NOT ["%errorlevel%"]==["0"] goto:error
+			
+			if "%~1" == "" goto:done
+	
+			timeout /t 3
+			shift
+			goto:PRProxy
 
 	:PRStandard
-		
+		cls
+		echo ╔═══════════════════════════════════════════════════════════════════════╗
+		echo ║  ooooooooo.                      ooooooooo.                           ║ 
+		echo ║  `888   `Y88.                    `888   `Y88.                         ║ 
+		echo ║   888   .d88' oooo d8b  .ooooo.   888   .d88'  .ooooo.   .oooo.o      ║ 
+		echo ║   888ooo88P'  `888""8P d88' `88b  888ooo88P'  d88' `88b d88(  "8      ║ 
+		echo ║   888          888     888   888  888`88b.    888ooo888 `"Y88b.       ║ 
+		echo ║   888          888     888   888  888  `88b.  888    .o o.  )88b      ║ 
+		echo ║  o888o        d888b    `Y8bod8P' o888o  o888o `Y8bod8P' 8""888P'      ║
+		echo ║                                                                       ║
+		echo ║        .o     .oooo.     .oooo.                                       ║
+		echo ║      .d88   .dP""Y88b  .dP""Y88b                                      ║
+		echo ║    .d'888         ]8P'       ]8P'                                     ║
+		echo ║  .d'  888       .d8P'      .d8P'                                      ║
+		echo ║  88ooo888oo   .dP'       .dP'                                         ║
+		echo ║       888   .oP     .o .oP     .o                                     ║
+		echo ║      o888o  8888888888 8888888888                                     ║
+		echo ╚═══════════════════════════════════════════════════════════════════════╝
+		echo.
 		echo [101;93m ENCODING... [0m
-		echo. && echo.
+		echo Input: %~1
+		echo Output: %~dp1%~n1_ProRes422.mov
+		echo.
 
 		::	Get codec name
         setlocal EnableDelayedExpansion
@@ -121,19 +164,46 @@ cls
 			-i "%~1" ^
 			-c:v prores_ks ^
 			-profile:v 2 ^
+			-quant_mat auto ^
 			-vendor apl0 ^
-			-bits_per_mb 8000 ^
+			-bits_per_mb 875 ^
 			-pix_fmt yuv422p10le ^
 			-c:a %codec_audio% ^
 			-map_metadata 0 ^
 			-movflags use_metadata_tags ^
 			"%~dp1%~n1_ProRes422.mov"
-			GOTO:ENDOFPRORES
+			if NOT ["%errorlevel%"]==["0"] goto:error
+			
+			if "%~1" == "" goto:done
+	
+			timeout /t 3
+			shift
+			goto:PRStandard
 				
 	:PR422HQ
-		
+		cls
+		echo ╔═════════════════════════════════════════════════════════════════════╗
+		echo ║  ooooooooo.                      ooooooooo.                         ║ 
+		echo ║  `888   `Y88.                    `888   `Y88.                       ║ 
+		echo ║   888   .d88' oooo d8b  .ooooo.   888   .d88'  .ooooo.   .oooo.o    ║ 
+		echo ║   888ooo88P'  `888""8P d88' `88b  888ooo88P'  d88' `88b d88(  "8    ║ 
+		echo ║   888          888     888   888  888`88b.    888ooo888 `"Y88b.     ║ 
+		echo ║   888          888     888   888  888  `88b.  888    .o o.  )88b    ║ 
+		echo ║  o888o        d888b    `Y8bod8P' o888o  o888o `Y8bod8P' 8""888P'    ║
+		echo ║                                                                     ║
+		echo ║        .o     .oooo.     .oooo.      ooooo   ooooo   .oooooo.       ║
+		echo ║      .d88   .dP""Y88b  .dP""Y88b     `888'   `888'  d8P'  `Y8b      ║
+		echo ║    .d'888         ]8P'       ]8P'     888     888  888      888     ║
+		echo ║  .d'  888       .d8P'      .d8P'      888ooooo888  888      888     ║
+		echo ║  88ooo888oo   .dP'       .dP'         888     888  888      888     ║
+		echo ║       888   .oP     .o .oP     .o     888     888  `88b    d88b     ║
+		echo ║      o888o  8888888888 8888888888    o888o   o888o  `Y8bood8P'Ybd'  ║
+		echo ╚═════════════════════════════════════════════════════════════════════╝
+		echo.		
 		echo [101;93m ENCODING... [0m
-		echo. && echo.
+		echo Input: %~1
+		echo Output: %~dp1%~n1_ProRes422HQ.mov
+		echo.
 
 		::	Get codec name
         setlocal EnableDelayedExpansion
@@ -155,19 +225,46 @@ cls
 			-i "%~1" ^
 			-c:v prores_ks ^
 			-profile:v 3 ^
+			-quant_mat auto ^
 			-vendor apl0 ^
-			-bits_per_mb 8000 ^
+			-bits_per_mb 1350 ^
 			-pix_fmt yuv422p10le ^
 			-c:a %codec_audio% ^
 			-map_metadata 0 ^
 			-movflags use_metadata_tags ^
 			"%~dp1%~n1_ProRes422HQ.mov"
-			GOTO:ENDOFPRORES
+			if NOT ["%errorlevel%"]==["0"] goto:error
+			
+			if "%~1" == "" goto:done
+	
+			timeout /t 3
+			shift
+			goto:PR422HQ
 				
 	:PR4444
-		
+		cls
+		echo ╔════════════════════════════════════════════════════════════════════╗
+		echo ║  ooooooooo.                      ooooooooo.                        ║ 
+		echo ║  `888   `Y88.                    `888   `Y88.                      ║ 
+		echo ║   888   .d88' oooo d8b  .ooooo.   888   .d88'  .ooooo.   .oooo.o   ║ 
+		echo ║   888ooo88P'  `888""8P d88' `88b  888ooo88P'  d88' `88b d88(  "8   ║ 
+		echo ║   888          888     888   888  888`88b.    888ooo888 `"Y88b.    ║ 
+		echo ║   888          888     888   888  888  `88b.  888    .o o.  )88b   ║ 
+		echo ║  o888o        d888b    `Y8bod8P' o888o  o888o `Y8bod8P' 8""888P'   ║
+		echo ║                                                                    ║
+		echo ║        .o         .o         .o         .o                         ║
+		echo ║      .d88       .d88       .d88       .d88                         ║
+		echo ║    .d'888     .d'888     .d'888     .d'888                         ║
+		echo ║  .d'  888   .d'  888   .d'  888   .d'  888                         ║
+		echo ║  88ooo888oo 88ooo888oo 88ooo888oo 88ooo888oo                       ║
+		echo ║       888        888        888        888                         ║
+		echo ║      o888o      o888o      o888o      o888o                        ║
+		echo ╚════════════════════════════════════════════════════════════════════╝
+		echo.
 		echo [101;93m ENCODING... [0m
-		echo. && echo.
+		echo Input: %~1
+		echo Output: %~dp1%~n1_ProRes4444.mov
+		echo.
 
 		::	Get codec name
         setlocal EnableDelayedExpansion
@@ -189,6 +286,7 @@ cls
 			-i "%~1" ^
 			-c:v prores_ks ^
 			-profile:v 4 ^
+			-quant_mat auto ^
 			-vendor apl0 ^
 			-bits_per_mb 8000 ^
 			-pix_fmt yuva444p10le ^
@@ -196,12 +294,38 @@ cls
 			-map_metadata 0 ^
 			-movflags use_metadata_tags ^
 			"%~dp1%~n1_ProRes4444.mov"
-			GOTO:ENDOFPRORES
+			if NOT ["%errorlevel%"]==["0"] goto:error
+			
+			if "%~1" == "" goto:done
+	
+			timeout /t 3
+			shift
+			goto:PR4444
 				
 	:PR4444XQ
-		
+		cls
+		echo ╔═════════════════════════════════════════════════════════════════════════════════╗
+		echo ║  ooooooooo.                      ooooooooo.                                     ║ 
+		echo ║  `888   `Y88.                    `888   `Y88.                                   ║ 
+		echo ║   888   .d88' oooo d8b  .ooooo.   888   .d88'  .ooooo.   .oooo.o                ║ 
+		echo ║   888ooo88P'  `888""8P d88' `88b  888ooo88P'  d88' `88b d88(  "8                ║ 
+		echo ║   888          888     888   888  888`88b.    888ooo888 `"Y88b.                 ║ 
+		echo ║   888          888     888   888  888  `88b.  888    .o o.  )88b                ║ 
+		echo ║  o888o        d888b    `Y8bod8P' o888o  o888o `Y8bod8P' 8""888P'                ║
+		echo ║                                                                                 ║
+		echo ║        .o         .o         .o         .o      ooooooo  ooooo   .oooooo.       ║
+		echo ║      .d88       .d88       .d88       .d88       `8888    d8'   d8P'  `Y8b      ║
+		echo ║    .d'888     .d'888     .d'888     .d'888         Y888..8P    888      888     ║
+		echo ║  .d'  888   .d'  888   .d'  888   .d'  888          `8888'     888      888     ║
+		echo ║  88ooo888oo 88ooo888oo 88ooo888oo 88ooo888oo       .8PY888.    888      888     ║
+		echo ║       888        888        888        888        d8'  `888b   `88b    d88b     ║
+		echo ║      o888o      o888o      o888o      o888o     o888o  o88888o  `Y8bood8P'Ybd'  ║
+		echo ╚═════════════════════════════════════════════════════════════════════════════════╝
+		echo.
 		echo [101;93m ENCODING... [0m
-		echo. && echo.
+		echo Input: %~1
+		echo Output: %~dp1%~n1_ProRes4444XQ.mov
+		echo.
 
 		::	Get codec name
         setlocal EnableDelayedExpansion
@@ -223,6 +347,7 @@ cls
 			-i "%~1" ^
 			-c:v prores_ks ^
 			-profile:v 5 ^
+			-quant_mat auto ^
 			-vendor apl0 ^
 			-bits_per_mb 8000 ^
 			-pix_fmt yuva444p10le ^
@@ -230,29 +355,29 @@ cls
 			-map_metadata 0 ^
 			-movflags use_metadata_tags ^
 			"%~dp1%~n1_ProRes4444XQ.mov"
-			GOTO:ENDOFPRORES
 
-	:ENDOFPRORES
-	if NOT ["%errorlevel%"]==["0"] goto:error
-	echo [92m%~n1 Done![0m
-	title FFMPEG - We did it!
-
-	if "%~1" == "" goto:done
+			if NOT ["%errorlevel%"]==["0"] goto:error
+			
+			if "%~1" == "" goto:done
 	
-	timeout /t 3
-	
-	shift
-	goto %choice%
+			timeout /t 3
+			shift
+			goto:PR4444XQ
 
 
 :errorfile
 	
 	cls
-	echo.
-	echo  [93m╔══════════════════╗
-	echo  [93m║====ATTENTION!====║
-	echo  [93m╚══════════════════╝
-	echo.
+	echo [93m╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
+	echo [93m║        .o.       ooooooooooooo ooooooooooooo oooooooooooo ooooo      ooo ooooooooooooo ooooo   .oooooo.   ooooo      ooo  ║
+	echo [93m║       .888.      8'   888   `8 8'   888   `8 `888'     `8 `888b.     `8' 8'   888   `8 `888'  d8P'  `Y8b  `888b.     `8'  ║
+	echo [93m║      .8"888.          888           888       888          8 `88b.    8       888       888  888      888  8 `88b.    8   ║
+	echo [93m║     .8' `888.         888           888       888oooo8     8   `88b.  8       888       888  888      888  8   `88b.  8   ║
+	echo [93m║    .88ooo8888.        888           888       888    "     8     `88b.8       888       888  888      888  8     `88b.8   ║
+	echo [93m║   .8'     `888.       888           888       888       o  8       `888       888       888  `88b    d88'  8       `888   ║
+	echo [93m║  o88o     o8888o     o888o         o888o     o888ooooood8 o8o        `8      o888o     o888o  `Y8bood8P'  o8o        `8   ║
+    echo [93m╚═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝                                                                                                                    
+    echo.
 	echo  [93mA file with the same name as
 	echo  [93mthe requested conversion output already exists.
 	echo.
