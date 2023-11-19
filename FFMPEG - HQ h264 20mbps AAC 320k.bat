5c5c18d7b7fcbe736023fd3ee93d1ad329fb7bae
littlemars-guy <81258511+littlemars-guy@users.noreply.github.com>
2023-11-20 00:27:54 +0100

    Update FFMPEG - HQ h264 20mbps AAC 320k.bat
    2023-11-19 Version 0.3:
    - Added VALIDATE_OUTPUT subroutine
    - Extended timeout for :ERROR_CHOICE from 10s to 30s

--- FFMPEG - HQ h264 20mbps AAC 320k.bat
+++ FFMPEG - HQ h264 20mbps AAC 320k.bat
@@ -4,6 +4,9 @@
 ::	What follows is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
 ::
 ::	---CHANGELOG-----------------------------------------------------------------------------------
+::	2023-11-20 Version 0.3
+::		- Added VALIDATE_OUTPUT subroutine
+::		- Extended timeout for :ERROR_CHOICE from 10s to 30s
 ::	2023-11-16 Version 0.2.1
 ::		Corrected behavior when shifting to next file
 ::	2023-11-10 Version 0.2
@@ -20,15 +23,51 @@ cls
 	title FFMPEG - Converting "%~1" to 20mbps h264 video with 320kbps AAC audio
 
     :: Check if output file already exists
-	    if exist "%~1_20mps_h264_256k_aac.mp4" goto:errorfile
-	    if "%~1" == "" goto:done
+    :: Check if output file already exists
+	set count=2
+	set OUTPUT_DIR=%~dp1
+	set OUTPUT_NAME=%~n1
+	set OUTPUT_ENC=-h264-20mbps-AAC320k
+	set OUTPUT_SFX=
+	set OUTPUT_EXT=.mp4
+	
+	:VALIDATE_OUTPUT
+		echo.
+		set OUTPUT_FILE="%OUTPUT_DIR%%OUTPUT_NAME%%OUTPUT_ENC%%OUTPUT_EXT%"
+		echo [101;93m VALIDATING OUTPUT... [0m
+			IF EXIST %OUTPUT_FILE% (
+				echo Output [30;41m UNAVAILABLE [0m && goto:errorfile
+			) ELSE ( 
+				echo Output [30;42m AVAILABLE [0m && goto:encode
+			)
+	:errorfile
+		set OUTPUT_SFX= (%count%)
+		set OUTPUT_FILE="%OUTPUT_DIR%%OUTPUT_NAME%%OUTPUT_ENC%%OUTPUT_SFX%%OUTPUT_EXT%"
+		IF EXIST %OUTPUT_FILE% (
+  	      set /A count+=1 && set OUTPUT_SFX= (%count%) && goto :errorfile
+ 	   ) ELSE ( 
+			goto :error_choice
+		)
+	:error_choice
+		echo.
+		echo [93mA file with the same name as the requested conversion output already exists.
+		echo [1mSelect the desired action:[0m
+		echo [33m[1][0m. Overwrite output (will ask again for confirmation)
+		echo [33m[2][0m. Rename output %OUTPUT_NAME%%OUTPUT_ENC%[30;43m-(%count%)[0m.%OUTPUT_EXT%[0m
+		echo [33m[3][0m. Abort the operation (will be auto-selected in 30s)
+		echo.
+	
+		CHOICE /C 123 /T 30 /D 3 /M "Enter your choice:"
+		:: Note - list ERRORLEVELS in decreasing order
+		IF ERRORLEVEL 3 goto :abort
+		IF ERRORLEVEL 2 goto :encode
+		IF ERRORLEVEL 1 EXIT /B
 
-    ::	Let's go!
+	:encode
 	    echo.
 	    echo [92m╔══════════════════════════════════════════════════════╗
 	    echo [92m║============== CONVERSION IN PROGRESS ================║
 	    echo [92m╚══════════════════════════════════════════════════════╝[0m
-		color 0E
 		echo.
 		echo.
 		echo.
@@ -49,12 +88,21 @@ cls
 		    -level 4.1 ^
 			-pix_fmt yuv420p ^
 			-c:a aac ^
-    	    -b:a 256k ^
+    	    -b:a 320k ^
     	    -map_metadata 0 ^
 			-movflags use_metadata_tags ^
     	    -movflags +faststart ^
-			"%~dp1%~n1-h264-20mbps-AAC256k.mp4"
-        goto:end
+			"%~dp1%~n1-h264-20mbps-AAC320k%OUTPUT_SFX%.mp4"
+	
+	set OUTPUT_SFX=""
+
+    if NOT ["%errorlevel%"]==["0"] goto:error
+	
+	echo [92m%~n1 Done![0m
+	shift
+	if "%~1" == "" goto:done
+	timeout /t 3 > nul
+	goto:next
 ::	DUAL PASS (DEPRECATED)
 	::	ffmpeg ^
 	::		-hwaccel auto ^
@@ -100,36 +148,11 @@ cls
 	::
 	::		goto:end
 
-:errorfile
-	
-	cls
-	echo.
-	echo  [93m╔══════════════════╗
-	echo  [93m║====ATTENTION!====║
-	echo  [93m╚══════════════════╝
-	echo.
-	echo  [93mA file with the same name as
-	echo  [93mthe requested conversion output already exists.
-	echo.
-	echo  [93mCheck the output folder before trying again!
-	echo.
-	pause
-	goto:end
-
 :error
 	
 	echo [93mThere was an error. Please check your input file.[0m
 	pause
 	exit 0
-
-:end
-    if NOT ["%errorlevel%"]==["0"] goto:error
-	echo [92m%~n1 Done![0m
-	title FFMPEG - We did it!
-	shift
-	if "%~1" == "" goto:done
-	timeout /t 3 > nul
-	goto:next
 
 :done
     
@@ -150,3 +173,22 @@ cls
     timeout /t 1 > nul
     exit 0
 
+:abort
+	
+	echo [93mProcess aborted.[0m
+	cls
+	echo [93mThis window will close after 5 seconds.[0m
+	timeout /t 1 > nul
+	cls
+	echo [93mThis window will close after 4 seconds.[0m
+	timeout /t 1 > nul
+	cls
+	echo [93mThis window will close after 3 seconds.[0m
+	timeout /t 1 > nul
+	cls
+	echo [93mThis window will close after 2 seconds.[0m
+	timeout /t 1 > nul
+	cls
+	echo [93mThis window will close after 1 seconds.[0m
+	timeout /t 1 > nul
+	exit 0
