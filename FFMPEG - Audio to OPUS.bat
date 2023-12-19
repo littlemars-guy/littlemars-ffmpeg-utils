85b3e7ad01f5ecc7ad73284d9cb74f197e761176
littlemars-guy <81258511+littlemars-guy@users.noreply.github.com>
2023-12-19 15:40:30 +0100

    Update FFMPEG - Audio to OPUS.bat
    Version 0.1.1
    - Moved banners to :banner subroutine

--- FFMPEG - Audio to OPUS.bat
+++ FFMPEG - Audio to OPUS.bat
@@ -7,37 +7,44 @@
 ::  Fancy font is "roman" from https://devops.datenkollektiv.de/banner.txt/index.html
 ::
 ::	---CHANGELOG-----------------------------------------------------------------------------------
+::	2023-12-06 Version 0.1.1
+::		- Moved banners to :banner subroutine
 ::	2023-11-23 Version 0.1
 ::		- Initial release -- [based on Audio to FLAC v0.4] --
+::
+::	---Debug Utils (will be removed in future releases---------------------------------------------
+::	if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 ::	-----------------------------------------------------------------------------------------------
-::if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
 @echo off
 chcp 65001
-cls
 setlocal EnableDelayedExpansion
+cls
+
+set file_count=0
 
 :again
+	set /A file_count+=1
+	if NOT DEFINED jump goto :next
+
+:jump
+	timeout /t 2 > nul
+    shift
+    if "%~1" == "" goto :done
+
+:next
+	set jump=yes
 	set file=%~1
 	set OUTPUT_DIR=%~dp1
 	set OUTPUT_NAME=%~n1
 	set %OUTPUT_SFX%=""
 	set OUTPUT_EXT=opus
 	set count=2
+	cls
 	title FFMPEG - Extracting audio from %file% to opus
 	echo.[0m
-	echo ╔═══════════════════════════════════════════════════════╗
-	echo ║    .oooooo.   ooooooooo.   ooooo     ooo  .oooooo..o  ║
-	echo ║   d8P'  `Y8b  `888   `Y88. `888'     `8' d8P'    `Y8  ║
-	echo ║  888      888  888   .d88'  888       8  Y88bo.       ║
-	echo ║  888      888  888ooo88P'   888       8   `"Y8888o.   ║
-	echo ║  888      888  888          888       8       `"Y88b  ║
-	echo ║  `88b    d88'  888          `88.    .8'  oo     .d8P  ║
-	echo ║   `Y8bood8P'  o888o           `YbodP'    8""88888P'   ║
-	echo ╚═══════════════════════════════════════════════════════╝ 
+	CALL :banner
 	echo.
-    echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
-	echo.
-	IF DEFINED choice (goto:analisys) ELSE (goto:preset_selection)
+	IF DEFINED choice (goto :analisys) ELSE (goto :preset_selection)
 	echo [101;93m PRESET SELECTION [0m
 
 :preset_selection
@@ -49,9 +56,9 @@ setlocal EnableDelayedExpansion
 	
 	CHOICE /C 123 /M "Enter your choice:"
 	:: Note - list ERRORLEVELS in decreasing order
-	IF ERRORLEVEL 3 set choice="medium" && set Preset=[Opus] && GOTO:analisys
-	IF ERRORLEVEL 2 set choice="VOIP" && set Preset=[Opus-VOIP] && GOTO:analisys
-	IF ERRORLEVEL 1 set choice="MUSIC" && set Preset=[Opus-MUSIC] && GOTO:analisys
+	IF ERRORLEVEL 3 set choice="medium" && set Preset=[Opus] && goto :analisys
+	IF ERRORLEVEL 2 set choice="VOIP" && set Preset=[Opus-VOIP] && goto :analisys
+	IF ERRORLEVEL 1 set choice="MUSIC" && set Preset=[Opus-MUSIC] && goto :analisys
 
 :analisys
 	::	Have we already been here?
@@ -79,9 +86,9 @@ setlocal EnableDelayedExpansion
 	set OUTPUT_FILE="%OUTPUT_DIR%%OUTPUT_NAME%-%Preset%.%OUTPUT_EXT%"
 	echo [101;93m VALIDATING OUTPUT... [0m
 		IF EXIST %OUTPUT_FILE% (
-   			echo Output [30;41m UNAVAILABLE [0m && goto:errorfile
+   			echo Output [30;41m UNAVAILABLE [0m && goto :errorfile
  		) ELSE ( 
-    		echo Output [30;42m AVAILABLE [0m && goto:encode
+    		echo Output [30;42m AVAILABLE [0m && goto :encode
 		)
 :errorfile
 	set OUTPUT_SFX= (%count%)
@@ -107,76 +114,61 @@ setlocal EnableDelayedExpansion
 	IF ERRORLEVEL 1 EXIT /B
 
 :encode
-	if "%choice%"=="MUSIC" goto:music
-	if "%choice%"=="VOIP" goto:voip
-	if "%choice%"=="medium" goto:medium
+	if "%choice%"=="MUSIC" goto :music
+	if "%choice%"=="VOIP" goto :voip
+	if "%choice%"=="medium" goto :medium
 	echo.
 	echo.
 	echo.
 	echo [101;93m ENCODING... [0m
+	echo File #%file_count%
+	echo Input: %~nx1
+	echo Output: %OUTPUT_NAME%%OUTPUT_SFX%.%OUTPUT_EXT%
 	echo.
 	:music
 		ffmpeg ^
-			-hide_banner ^
-			-loglevel warning ^
-			-stats -i "%~1" ^
-			-map 0:a ^
-			-vn ^
-			-c:a libopus ^
-			-b:a 256k ^
-			-application audio ^
-			-apply_phase_inv 1 ^
-			-cutoff 0 ^
-			-map_metadata 0 ^
-			-write_id3v2 1 ^
+			-hide_banner -loglevel warning -stats ^
+			-i "%~1" ^
+			-map 0:a -vn ^
+			-c:a libopus -b:a 256k ^
+			-application audio -apply_phase_inv 1 -cutoff 0 ^
+			-map_metadata 0 -write_id3v2 1 ^
 			"%~dp1%~n1-%Preset%%OUTPUT_SFX%.%OUTPUT_EXT%"
-
-		goto:next
+		
+		if NOT ["%errorlevel%"]==["0"] set print_error_level=%errorlevel% && goto :error		
+		echo [92m%~n1 Done![0m
+		goto :again
 
 	:voip
 		ffmpeg ^
-			-hide_banner ^
-			-loglevel warning ^
+			-hide_banner -loglevel warning ^
 			-stats -i "%~1" ^
-			-map 0:a ^
-			-vn ^
-			-c:a libopus ^
-			-b:a 64k ^
-			-application voip ^
-			-apply_phase_inv 0 ^
-			-cutoff 20000 ^
+			-map 0:a -vn ^
+			-c:a libopus -b:a 64k ^
+			-application voip -apply_phase_inv 0 -cutoff 20000 ^
 			-ac 1 ^
-			-map_metadata 0 ^
-			-write_id3v2 1 ^
+			-map_metadata 0 -write_id3v2 1 ^
 			"%~dp1%~n1-%Preset%%OUTPUT_SFX%.%OUTPUT_EXT%"
 		
-		goto:next
+		if NOT ["%errorlevel%"]==["0"] set print_error_level=%errorlevel% && goto :error		
+		echo [92m%~n1 Done![0m
+
+		goto :again
 
 	:medium
 		ffmpeg ^
-			-hide_banner ^
-			-loglevel warning ^
-			-stats -i "%~1" ^
-			-map 0:a ^
-			-vn ^
-			-c:a libopus ^
-			-b:a 128k ^
-			-application audio ^
-			-apply_phase_inv 1 ^
-			-cutoff 0 ^
-			-map_metadata 0 ^
-			-write_id3v2 1 ^
+			-hide_banner -loglevel warning -stats ^
+			-i "%~1" ^
+			-map 0:a -vn ^
+			-c:a libopus -b:a 128k ^
+			-application audio -apply_phase_inv 1 -cutoff 0 ^
+			-map_metadata 0 -write_id3v2 1 ^
 			"%~dp1%~n1-%Preset%%OUTPUT_SFX%.%OUTPUT_EXT%"
+
+		if NOT ["%errorlevel%"]==["0"] set print_error_level=%errorlevel% && goto :error		
+		echo [92m%~n1 Done![0m
 		
-		goto:next
-
-:next
-	if NOT ["%errorlevel%"]==["0"] goto:error
-	echo [92m%~n1 Done![0m
-
-	shift
-	if "%~1" == "" goto:end
-	goto:again
+		goto :again	
 
 :error_same_codec
 	
@@ -190,9 +182,9 @@ setlocal EnableDelayedExpansion
 
 	CHOICE /t 10 /C 123 /D 1 /M "Enter your choice:"
 	:: Note - ERRORLEVELS are listed in decreasing order
-	IF ERRORLEVEL 3 goto:abort
-	IF ERRORLEVEL 2 goto:analisys
-	IF ERRORLEVEL 1 set same_codec_remember=yes && goto:again
+	IF ERRORLEVEL 3 goto :abort
+	IF ERRORLEVEL 2 goto :analisys
+	IF ERRORLEVEL 1 set same_codec_remember=yes && goto :again
 
 :errorbits32
 	
@@ -201,115 +193,39 @@ setlocal EnableDelayedExpansion
 	exit 0
 
 :error
-	
 	echo [93mThere was an error. Please check your input file.[0m
+	echo Errorlevel is %print_error_level%
 	pause
-	exit 0
+	goto :eof
 
 :abort
-	
-	cls
-	echo.[0m
-	echo ╔═══════════════════════════════════════════════════════╗
-	echo ║    .oooooo.   ooooooooo.   ooooo     ooo  .oooooo..o  ║
-	echo ║   d8P'  `Y8b  `888   `Y88. `888'     `8' d8P'    `Y8  ║
-	echo ║  888      888  888   .d88'  888       8  Y88bo.       ║
-	echo ║  888      888  888ooo88P'   888       8   `"Y8888o.   ║
-	echo ║  888      888  888          888       8       `"Y88b  ║
-	echo ║  `88b    d88'  888          `88.    .8'  oo     .d8P  ║
-	echo ║   `Y8bood8P'  o888o           `YbodP'    8""88888P'   ║
-	echo ╚═══════════════════════════════════════════════════════╝ 
-	echo.
-    echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
-	echo.
-	echo.
-	echo [30;41mProcess aborted.[0m
-	echo.
-	echo [93mThis window will close after 5 seconds.[0m
-	timeout /t 1 > nul
-	cls
-	echo.[0m
-	echo ╔═══════════════════════════════════════════════════════╗
-	echo ║    .oooooo.   ooooooooo.   ooooo     ooo  .oooooo..o  ║
-	echo ║   d8P'  `Y8b  `888   `Y88. `888'     `8' d8P'    `Y8  ║
-	echo ║  888      888  888   .d88'  888       8  Y88bo.       ║
-	echo ║  888      888  888ooo88P'   888       8   `"Y8888o.   ║
-	echo ║  888      888  888          888       8       `"Y88b  ║
-	echo ║  `88b    d88'  888          `88.    .8'  oo     .d8P  ║
-	echo ║   `Y8bood8P'  o888o           `YbodP'    8""88888P'   ║
-	echo ╚═══════════════════════════════════════════════════════╝ 
-	echo.
-    echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
-	echo.
-	echo.
-	echo [30;41mProcess aborted.[0m
-	echo.
-	echo [93mThis window will close after 4 seconds.[0m
-	timeout /t 1 > nul
-	cls
-	echo.[0m
-	echo ╔═══════════════════════════════════════════════════════╗
-	echo ║    .oooooo.   ooooooooo.   ooooo     ooo  .oooooo..o  ║
-	echo ║   d8P'  `Y8b  `888   `Y88. `888'     `8' d8P'    `Y8  ║
-	echo ║  888      888  888   .d88'  888       8  Y88bo.       ║
-	echo ║  888      888  888ooo88P'   888       8   `"Y8888o.   ║
-	echo ║  888      888  888          888       8       `"Y88b  ║
-	echo ║  `88b    d88'  888          `88.    .8'  oo     .d8P  ║
-	echo ║   `Y8bood8P'  o888o           `YbodP'    8""88888P'   ║
-	echo ╚═══════════════════════════════════════════════════════╝ 
-	echo.
-    echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
-	echo.
-	echo.
-	echo [30;41mProcess aborted.[0m
-	echo.
-	echo [93mThis window will close after 3 seconds.[0m
-	timeout /t 1 > nul
-	cls
-	echo.[0m
-	echo ╔═══════════════════════════════════════════════════════╗
-	echo ║    .oooooo.   ooooooooo.   ooooo     ooo  .oooooo..o  ║
-	echo ║   d8P'  `Y8b  `888   `Y88. `888'     `8' d8P'    `Y8  ║
-	echo ║  888      888  888   .d88'  888       8  Y88bo.       ║
-	echo ║  888      888  888ooo88P'   888       8   `"Y8888o.   ║
-	echo ║  888      888  888          888       8       `"Y88b  ║
-	echo ║  `88b    d88'  888          `88.    .8'  oo     .d8P  ║
-	echo ║   `Y8bood8P'  o888o           `YbodP'    8""88888P'   ║
-	echo ╚═══════════════════════════════════════════════════════╝ 
-	echo.
-    echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
-	echo.
-	echo.
-	echo [30;41mProcess aborted.[0m
-	echo.
-	echo [93mThis window will close after 2 seconds.[0m
-	timeout /t 1 > nul
-	cls
-	echo.[0m
-	echo ╔═══════════════════════════════════════════════════════╗
-	echo ║    .oooooo.   ooooooooo.   ooooo     ooo  .oooooo..o  ║
-	echo ║   d8P'  `Y8b  `888   `Y88. `888'     `8' d8P'    `Y8  ║
-	echo ║  888      888  888   .d88'  888       8  Y88bo.       ║
-	echo ║  888      888  888ooo88P'   888       8   `"Y8888o.   ║
-	echo ║  888      888  888          888       8       `"Y88b  ║
-	echo ║  `88b    d88'  888          `88.    .8'  oo     .d8P  ║
-	echo ║   `Y8bood8P'  o888o           `YbodP'    8""88888P'   ║
-	echo ╚═══════════════════════════════════════════════════════╝ 
-	echo.
-    echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
-	echo.
-	echo.
-	echo [30;41mProcess aborted.[0m
-	echo.
-	echo [93mThis window will close after 1 seconds.[0m
-	timeout /t 1 > nul
-	exit 0
+	set countdown=5
+	CALL :abort_cycle
+	:abort_cycle
+		cls
+		CALL :banner
+		echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo.
+		echo [93mProcess aborted.[0m
+		set /A countdown-=1
+		timeout /t 1 > nul
+		if "%countdown%"=="0" exit 0
+		goto :abort_cycle
 
 
-:end
-	
-	cls
-	echo.[0m
+:done
+	set countdown=5
+	CALL :end_cycle
+	:end_cycle
+		cls
+		CALL :banner
+		echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo. && echo.
+		echo [92mEncoding succesful. This window will close after %countdown% seconds.[0m
+		set /A countdown-=1
+		timeout /t 1 > nul
+		if "%countdown%"=="0" exit 0
+		goto :end_cycle
+
+:banner
 	echo ╔═══════════════════════════════════════════════════════╗
 	echo ║    .oooooo.   ooooooooo.   ooooo     ooo  .oooooo..o  ║
 	echo ║   d8P'  `Y8b  `888   `Y88. `888'     `8' d8P'    `Y8  ║
@@ -321,76 +237,4 @@ setlocal EnableDelayedExpansion
 	echo ╚═══════════════════════════════════════════════════════╝ 
 	echo.
     echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
-	echo.
-	echo.
-	echo [92mEncoding succesful. This window will close after 5 seconds.[0m
-	timeout /t 1 > nul
-	cls
-	echo.[0m
-	echo ╔═══════════════════════════════════════════════════════╗
-	echo ║    .oooooo.   ooooooooo.   ooooo     ooo  .oooooo..o  ║
-	echo ║   d8P'  `Y8b  `888   `Y88. `888'     `8' d8P'    `Y8  ║
-	echo ║  888      888  888   .d88'  888       8  Y88bo.       ║
-	echo ║  888      888  888ooo88P'   888       8   `"Y8888o.   ║
-	echo ║  888      888  888          888       8       `"Y88b  ║
-	echo ║  `88b    d88'  888          `88.    .8'  oo     .d8P  ║
-	echo ║   `Y8bood8P'  o888o           `YbodP'    8""88888P'   ║
-	echo ╚═══════════════════════════════════════════════════════╝ 
-	echo.
-    echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
-	echo.
-	echo.
-	echo [92mEncoding succesful. This window will close after 4 seconds.[0m
-	timeout /t 1 > nul
-	cls
-	echo.[0m
-	echo ╔═══════════════════════════════════════════════════════╗
-	echo ║    .oooooo.   ooooooooo.   ooooo     ooo  .oooooo..o  ║
-	echo ║   d8P'  `Y8b  `888   `Y88. `888'     `8' d8P'    `Y8  ║
-	echo ║  888      888  888   .d88'  888       8  Y88bo.       ║
-	echo ║  888      888  888ooo88P'   888       8   `"Y8888o.   ║
-	echo ║  888      888  888          888       8       `"Y88b  ║
-	echo ║  `88b    d88'  888          `88.    .8'  oo     .d8P  ║
-	echo ║   `Y8bood8P'  o888o           `YbodP'    8""88888P'   ║
-	echo ╚═══════════════════════════════════════════════════════╝ 
-	echo.
-    echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
-	echo.
-	echo.
-	echo [92mEncoding succesful. This window will close after 3 seconds.[0m
-	timeout /t 1 > nul
-	cls
-	echo.[0m
-	echo ╔═══════════════════════════════════════════════════════╗
-	echo ║    .oooooo.   ooooooooo.   ooooo     ooo  .oooooo..o  ║
-	echo ║   d8P'  `Y8b  `888   `Y88. `888'     `8' d8P'    `Y8  ║
-	echo ║  888      888  888   .d88'  888       8  Y88bo.       ║
-	echo ║  888      888  888ooo88P'   888       8   `"Y8888o.   ║
-	echo ║  888      888  888          888       8       `"Y88b  ║
-	echo ║  `88b    d88'  888          `88.    .8'  oo     .d8P  ║
-	echo ║   `Y8bood8P'  o888o           `YbodP'    8""88888P'   ║
-	echo ╚═══════════════════════════════════════════════════════╝ 
-	echo.
-    echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
-	echo.
-	echo.
-	echo [92mEncoding succesful. This window will close after 2 seconds.[0m
-	timeout /t 1 > nul
-	cls
-	echo.[0m
-	echo ╔═══════════════════════════════════════════════════════╗
-	echo ║    .oooooo.   ooooooooo.   ooooo     ooo  .oooooo..o  ║
-	echo ║   d8P'  `Y8b  `888   `Y88. `888'     `8' d8P'    `Y8  ║
-	echo ║  888      888  888   .d88'  888       8  Y88bo.       ║
-	echo ║  888      888  888ooo88P'   888       8   `"Y8888o.   ║
-	echo ║  888      888  888          888       8       `"Y88b  ║
-	echo ║  `88b    d88'  888          `88.    .8'  oo     .d8P  ║
-	echo ║   `Y8bood8P'  o888o           `YbodP'    8""88888P'   ║
-	echo ╚═══════════════════════════════════════════════════════╝ 
-	echo.
-    echo - This script is distributed under the GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 -
-	echo.
-	echo.
-	echo [92mEncoding succesful. This window will close after 1 seconds.[0m
-	timeout /t 1 > nul
-	exit 0
+	exit /B
